4b53f3105d78889017189f6c877f9fa1aecd4932
Nathan Hall <nathanhall762@gmail.com>
2024-01-23 14:13:22 -0600

    added placeholders

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,18 +1,25 @@
 interface HeroProps {
   welcomeText: string;
   tagline: string;
+  backgroundImage: string;
 }
 
-const Hero: React.FC<HeroProps> = ({ welcomeText, tagline }) => {
+const Hero: React.FC<HeroProps> = ({ welcomeText, tagline, backgroundImage }) => {
+  const heroStyle = {
+    backgroundImage: `url(${backgroundImage})`,
+    backgroundSize: 'cover',
+    backgroundPosition: 'center',
+    height: '100vh',
+  };
   return (
-    <>
+    <div style={heroStyle}>
       <div className='h-screen pl-16 pr-80 pt-96 text-left'>
-        <div className="fade-in-up" >
+        <div className="fade-in-up">
           <h2 className='text-2xl'>{welcomeText}</h2>
           <h1 className='mt-20 text-6xl tracking-widest'>{tagline}</h1>
         </div>
       </div>
-    </>
+    </div>
   );
 };
 
