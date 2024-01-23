9cbe327789d57086866f5ba4b2b0a622dfc0a2f3
EthanWakeford <ethanwakeford@gmail.com>
2024-01-23 16:28:12 -0600

    animation

--- src/components/StatsCard.tsx
+++ src/components/StatsCard.tsx
@@ -1,7 +1,12 @@
 import { useInViewport } from 'react-in-viewport';
 import { useRef } from 'react';
 
-const StatsCard: React.FC = () => {
+interface Props {
+  title: string;
+  body: string;
+}
+
+const StatsCard: React.FC<Props> = ({ body, title }) => {
   const ref = useRef(null);
   const { inViewport } = useInViewport(ref);
 
@@ -13,21 +18,8 @@ const StatsCard: React.FC = () => {
         ref={ref}
         className={`flex h-full flex-col items-center justify-evenly ${inViewport ? 'fade-in-up' : ''}`}
       >
-        <h3 className='text-xl'>Hello look at my stats</h3>
-        <p>
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ut cumque
-          tempora minus voluptatum officia nostrum, porro repudiandae, nesciunt
-          non obcaecati eum nobis velit! Molestiae dicta ipsa enim facilis
-          mollitia quos neque nisi quia! Mollitia labore aut, nostrum provident
-          velit sequi temporibus. Expedita corporis harum sint assumenda at
-          blanditiis aperiam consequuntur amet recusandae ratione, maiores iusto
-          officia et repellendus quasi natus earum omnis optio totam
-          perferendis, iure incidunt, quibusdam ab! Ad maxime pariatur quos non
-          molestias, minima quas placeat repellat quibusdam, accusantium nulla
-          dolorem aliquid labore saepe unde praesentium tempora dignissimos?
-          Aperiam libero culpa tempore architecto adipisci similique sint
-          quisquam in.
-        </p>
+        <h3 className='text-xl'>{title}</h3>
+        <p>{body}</p>
       </div>
     </div>
   );
