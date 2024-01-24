1a65348365c31ccb24440a14319564ca3c9bd884
EthanWakeford <ethanwakeford@gmail.com>
2024-01-23 22:29:41 -0600

    gallery now takes pictures

--- src/components/PictureGallery.tsx
+++ src/components/PictureGallery.tsx
@@ -6,8 +6,10 @@ const PictureGallery: React.FC<Props> = ({ images }) => {
   return (
     <ul className='align-center grid grid-cols-1 text-center sm:grid-cols-2'>
       {images.map((image) => (
-        <li className='flex h-32 items-center justify-center opacity-0 hover:opacity-100'>
-          <a href={image.link} className=''>
+        <li
+          className={`flex h-32 items-center justify-center bg-[url('${image.imageRef}')] group opacity-50 hover:opacity-100`}
+        >
+          <a href={image.link} className='invisible group-hover:visible'>
             {image.title}
           </a>
         </li>
