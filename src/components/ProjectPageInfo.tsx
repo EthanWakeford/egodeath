8a47888681b534a8590999bca144993de50d77b1
EthanWakeford <ethanwakeford@gmail.com>
2024-01-26 12:43:43 -0600

    updating project info component

--- src/components/ProjectPageInfo.tsx
+++ src/components/ProjectPageInfo.tsx
@@ -1,6 +1,7 @@
 interface Props {
   title: string;
   description: string;
+  date: string;
   company?: string;
   credits?: string[];
 }
@@ -8,6 +9,7 @@ interface Props {
 const ProjectPageInfo: React.FC<Props> = ({
   description,
   title,
+  date,
   company,
   credits,
 }) => {
@@ -17,12 +19,13 @@ const ProjectPageInfo: React.FC<Props> = ({
       <h3 className='text-md mt-4 text-center'>{company}</h3>
       <div className='grid grid-cols-4 pt-32'>
         <h2 className='col-span-3 px-4 text-xl'>{description}</h2>
-        <div className='col-span-1 px-4 '>
-          <h3 className=' text-center text-lg'>Credits</h3>
+        <div className='col-span-1 px-4 text-center text-lg'>
+          <h3 className=''>Credits</h3>
           <div className='flex flex-wrap justify-center gap-2'>
             {credits &&
-              credits.map((credit) => <p className='text-md'>{credit}</p>)}
+              credits.map((credit) => <p className='text-base'>{credit}</p>)}
           </div>
+          <h3 className=''>{date}</h3>
         </div>
       </div>
     </div>
