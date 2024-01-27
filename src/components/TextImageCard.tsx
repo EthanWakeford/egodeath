265bd1cda3c6056be7032db2e93c34003b50a264
EthanWakeford <ethanwakeford@gmail.com>
2024-01-26 18:52:28 -0600

    added revesed option for statsimagecard

--- src/components/TextImageCard.tsx
+++ src/components/TextImageCard.tsx
@@ -5,6 +5,7 @@ interface Props {
   bodyText: string;
   link: string;
   linkText: string;
+  reversed: boolean;
 }
 
 const TextImagecard: React.FC<Props> = ({
@@ -14,13 +15,14 @@ const TextImagecard: React.FC<Props> = ({
   bodyText,
   link,
   linkText,
+  reversed,
 }) => {
   return (
     <div className='grid h-[70vh] grid-rows-2 lg:h-[50vh] lg:grid-cols-3 lg:grid-rows-1'>
       <img
         src={imageLink}
         alt={imageAltText}
-        className='h-full w-full object-cover lg:col-span-2'
+        className={`${reversed && 'order-1'}  h-full w-full object-cover lg:col-span-2`}
       />
       <div className='flex flex-col justify-between px-4 py-8 text-center lg:col-span-1 lg:px-12'>
         <h3 className='text-lg lg:text-2xl'>{title}</h3>
