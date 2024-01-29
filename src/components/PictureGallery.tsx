f5f109b9ae71c5b0bc6af5d1d697f5187aa0f281
Nathan Hall <nathanhall762@gmail.com>
2024-01-29 11:51:20 -0600

    finished gallery modal

--- src/components/PictureGallery.tsx
+++ src/components/PictureGallery.tsx
@@ -1,30 +1,71 @@
+import { useState } from 'react';
+
 interface Props {
   images: { imageRef: string; title: string; link?: string }[];
 }
 
+interface SelectedImage {
+  imageRef: string;
+  title: string;
+  link?: string;
+}
+
 const PictureGallery: React.FC<Props> = ({ images }) => {
+  const [isModalOpen, setModalOpen] = useState(false);
+  const [selectedImage, setSelectedImage] = useState<SelectedImage | null>(
+    null
+  );
+
+  const openModal = (image: SelectedImage) => {
+    setSelectedImage(image);
+    setModalOpen(true);
+    console.log(isModalOpen);
+  };
+
+  const closeModal = () => {
+    setModalOpen(false);
+    setSelectedImage(null);
+  };
+
   return (
-    <ul className='align-center grid grid-cols-1 text-center sm:grid-cols-2'>
-      {images.map((image) => (
-        <li
-          className={`card-zoom group flex h-64 items-center justify-center bg-cover bg-center`}
-        >
-          <div
-            className='card-zoom-image hover:zoom-in hover:brightness-50'
-            style={{
-              backgroundImage: `url('${image.imageRef}')`,
-            }}
-          ></div>
-          <a
-            href={image.link ? image.link : image.imageRef}
-            target="_blank"
-            className='z-50 text-xl opacity-0 transition-opacity duration-300 ease-in-out group-hover:opacity-100'
+    <>
+      <ul className='align-center grid grid-cols-1 text-center sm:grid-cols-2'>
+        {images.map((image) => (
+          <li
+            className={`card-zoom group relative z-10 flex h-64 items-center justify-center bg-cover bg-center hover:cursor-pointer`}
+            onClick={() =>
+              image.link ? window.open(image.link) : openModal(image)
+            }
           >
-            {image.title}
-          </a>
-        </li>
-      ))}
-    </ul>
+            <div
+              className='card-zoom-image hover:zoom-in z-0 hover:brightness-50'
+              style={{ backgroundImage: `url('${image.imageRef}')` }}
+            ></div>
+            <span className='z-50 text-xl opacity-0 transition-opacity duration-300 ease-in-out group-hover:opacity-100'>
+              {image.title}
+            </span>
+          </li>
+        ))}
+      </ul>
+
+      {isModalOpen && (
+        <div className='fixed inset-0 z-40 flex items-center justify-center bg-black bg-opacity-50 p-0 lg:mt-8'>
+          <div className='z-50 m-auto rounded bg-white'>
+            {selectedImage && (
+              <div>
+                <i
+                  className='absolute lg:relative right-0 mx-4 flex cursor-pointer justify-end text-4xl text-black transition-transform duration-300 ease-in-out hover:text-red-500'
+                  onClick={closeModal}
+                >
+                  &times;
+                </i>
+                <img src={selectedImage.imageRef} alt={selectedImage.title} />
+              </div>
+            )}
+          </div>
+        </div>
+      )}
+    </>
   );
 };
 
