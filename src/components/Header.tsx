527bf9f9fd0e422435df11d4ae1421dc6e0abb1e
EthanWakeford <ethanwakeford@gmail.com>
2024-02-08 21:48:34 -0600

    made header smaller + fixed hover animations on project gallery

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -35,7 +35,7 @@ const Header: React.FC<HeaderProps> = ({ title, navs }) => {
 
   return (
     <header
-      className={`fixed top-0 z-40 flex w-screen justify-between px-4 py-2 text-center align-middle shadow-2xl transition-all duration-1000 lg:grid lg:grid-cols-5 lg:px-0 lg:py-8 ${isScrolled ? 'bg-opacity-100' : 'bg-opacity-50 dark:bg-opacity-50'} bg-ltbg1 lg:hover:bg-opacity-100 dark:bg-dkbg1`}
+      className={`fixed top-0 z-40 flex w-screen justify-between px-4 py-2 text-center align-middle shadow-2xl transition-all duration-1000 lg:grid lg:grid-cols-5 lg:px-0 lg:py-2 ${isScrolled ? 'bg-opacity-100' : 'bg-opacity-50 dark:bg-opacity-50'} bg-ltbg1 lg:hover:bg-opacity-100 dark:bg-dkbg1`}
     >
       <div className='items-center justify-center align-middle lg:col-span-1 lg:flex'>
         <h2 className='text-center text-2xl text-black transition-all duration-300 hover:text-accent dark:text-white'>
@@ -55,7 +55,7 @@ const Header: React.FC<HeaderProps> = ({ title, navs }) => {
       </nav>
       <div
         onClick={toggleNav}
-        className={`flex items-center justify-center z-50 w-10 cursor-pointer text-2xl text-black transition-transform duration-300 lg:hidden dark:text-white ${isNavOpen ? 'rotate-180 opacity-100' : 'rotate-0 opacity-50'}`}
+        className={`z-50 flex w-10 cursor-pointer items-center justify-center text-2xl text-black transition-transform duration-300 lg:hidden dark:text-white ${isNavOpen ? 'rotate-180 opacity-100' : 'rotate-0 opacity-50'}`}
       >
         {isNavOpen ? <Icon icon={closeIcon} /> : <Icon icon={menuIcon} />}
       </div>
